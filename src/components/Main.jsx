d875c3cdf4c753491f84ae5f29097e089abacf5f
Lulseged Admasu <lulsegedwork@gmail.comu>
2022-10-18 14:11:10 -0400

    basic meme app complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,36 +7,56 @@ export default function Main() {
     const memesData = Meme_Data.data.memes
     // const memeArr = memesData
     const[meme, setMeme] = useState({
-        topTxt: 'hi',
-        btmTxt: 'bye',
+        topTxt: '',
+        btmTxt: '',
         imgUrl: memesData[index].url
     })
-    // const [imgUrl, setImgUrl] = useState(memesData[index].url)
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prev => {
+            return {
+                ...prev,
+                [name]: value,
+            }
+        })
+    }
+    function getMeme() {
+        index = Math.floor(Math.random() * 100)
+        setMeme(prevState => (
+                {
+                ...prevState,
+                imgUrl: memesData[index].url
+            }
+        ))
+    }
+    
     return (
         <div className="main">
             <div className="top">
                 <div className="input flex">
-                    <input type="text" placeholder='text1'/>
-                    <input type="text" placeholder='text2'/>
+                    <input 
+                        type="text" 
+                        placeholder='text1' 
+                        name='topTxt' 
+                        value={meme.topTxt} 
+                        onChange={handleChange}
+                    />
+                    <input 
+                        type="text"
+                        placeholder='text2' 
+                        name='btmTxt' 
+                        value={meme.btmTxt} 
+                        onChange={handleChange}
+                    />
                 </div>
-                <button
-               onClick={
-                () => {
-                    index = Math.floor(Math.random() * 100)
-                    setMeme(prevState => (
-                         {
-                            ...prevState,
-                            imgUrl: memesData[index].url
-                        }
-                    ))
-                }
-               }
-               >
+                <button onClick={getMeme}>
                     get a new meme image <i className="fa-solid fa-image"></i>
                 </button>
             </div>
             <div className="bottom flex">
                 <img src={meme.imgUrl} />
+                <h2 className='ttxt'>{meme.topTxt}</h2>
+                <h2 className='btxt'>{meme.btmTxt}</h2>
             </div>
         </div>
     )
